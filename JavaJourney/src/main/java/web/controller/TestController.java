489b889d5a16b94925f6d447b60d9d75d68c2a1e
iro_oni <iro_oni@localhost>
2024-10-31 17:42:14 +0900

    241031 설정 및 test페이지 생성!

---
+++ JavaJourney/src/main/java/web/controller/TestController.java
@@ -0,0 +1,19 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequestMapping("/test")
+public class TestController {
+	
+	@GetMapping("/test")
+	public void test() {
+		log.info("test 접속!");
+	}
+	
+}
