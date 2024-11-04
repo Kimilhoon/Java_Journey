5e7c2ff03e20567da3302c133987826a63aeed9b
cheolhwa <goskflehd123@naver.com>
2024-11-04 14:38:55 +0900

    view 생성

--- JavaJourney/src/main/java/web/controller/CafeController.java
+++ JavaJourney/src/main/java/web/controller/CafeController.java
@@ -1,6 +1,8 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +14,22 @@ import web.service.impl.CafeServiceImpl;
 @Slf4j
 public class CafeController {
 	
-	private CafeService service = new CafeServiceImpl();
+	@Autowired private CafeService service = new CafeServiceImpl();
 	
-}
+	@GetMapping("/best")
+	public void BestCafeForm() {
+		
+	} // BestCafeForm() end
+
+	@GetMapping("/all")
+	public void AllCafeForm() {
+		
+	} // AllCafeForm() end
+
+//	@GetMapping("/info")
+//	public void CafeInfoForm(Cafe param) {
+//		
+//	} // CafeInfoForm(Cafe param) end
+	
+	
+} // class end
