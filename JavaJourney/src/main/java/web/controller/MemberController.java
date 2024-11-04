54888e9867a6393845a9b846dd14b710ea436cee
user1 <sbeei97@naver.com>
2024-11-04 17:17:45 +0900

    테스트

--- JavaJourney/src/main/java/web/controller/MemberController.java
+++ JavaJourney/src/main/java/web/controller/MemberController.java
@@ -1,19 +1,37 @@
 package web.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
+import web.dto.Member;
 import web.service.face.MemberService;
 import web.service.impl.MemberServiceImpl;
 
 @Controller
-@RequestMapping("/Member")
+@RequestMapping("/member")
 @Slf4j
 public class MemberController {
 	
 	@Autowired
 	private MemberService service = new MemberServiceImpl();
+	
+	@GetMapping("/login")
+	public void login() {}
+	
+	@PostMapping("/login")
+	public void loginForm(Member member, HttpSession session) {
+		log.info("login param : {}", member);
+		
+		//boolean isLogin = service.login(member);
+	}
+	
+	
+	
 	
 }
