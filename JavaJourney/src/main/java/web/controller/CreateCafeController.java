bcf933a4beba23511c6dc297e93c2e63d29bf62b
user1 <sbeei97@naver.com>
2024-11-07 14:50:39 +0900

    카페등록까지

--- JavaJourney/src/main/java/web/controller/CreateCafeController.java
+++ JavaJourney/src/main/java/web/controller/CreateCafeController.java
@@ -3,9 +3,11 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
+import web.dto.Cafe;
 import web.service.face.CreateCafeService;
 
 @Controller
@@ -17,5 +19,13 @@ public class CreateCafeController {
 	
 	@GetMapping("/cafe")
 	public void write() {}
+	
+	@PostMapping("/cafe")
+	public String writeProc(Cafe cafe) {
+		service.insertCafe(cafe);
+		
+		return "redirect:/manager/menu";
+	}
+	
 
 }
