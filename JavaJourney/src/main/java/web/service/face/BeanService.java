8015b205c9154790abafe4238cc6dcd3e208853c
철화 <goskflehd123@naver.com>
2024-11-11 18:04:21 +0900

    후후...

--- JavaJourney/src/main/java/web/service/face/BeanService.java
+++ JavaJourney/src/main/java/web/service/face/BeanService.java
@@ -11,18 +11,22 @@ public interface BeanService {
 	/**
 	 * 페이징 객체 생성
 	 * @param param - 요청 정보 객체 
+	 * @param keyword - 검색시 찾을 단어
+	 * @param cupnote - 카테고리 설정
 	 * 
 	 * @return 페이징 계산이 완료된 Paging 객체
 	 */
-	public Paging getPaging(Paging param);
+	public Paging getPaging(Paging param, String cupnote, String keyword);
 
 	
 	/**
 	 * 모든 원두 조회
-	 * @param paging 
+	 * @param paging - 카테고리, 검색어로 필터링한 페이징 객체
+	 * @param keyword - 검색어
+	 * @param cupnote - 카테고리
 	 * @return 조회한 원두
 	 */
-	public List<Bean> getAllBean(Paging paging);
+	public List<Bean> getAllBean(Paging paging, String cupnote, String keyword);
 
 
 	/**
